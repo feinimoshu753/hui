5875e6fb696a30e1285f0754c1dda697d40ea878
edward_hu <hello_world1024@163.com>
2018-05-07 23:07:38 +0800

    button定义

--- src/component/button/index.js
+++ src/component/button/index.js
@@ -4,10 +4,16 @@ import './index.css'
 export default class Button extends Component {
 
     render() {
+        let buttonClass = 'hui-button';
+        let buttonSizeClass = ` hui-button-size-${this.props.size}`;
+        let buttonTypeClass = ` hui-button-type-${this.props.type}`;
+        let buttonBorderType = this.props.borderType ? ' hui-button-border-type' : '';
+        let buttonDisabled = this.props.disabled ? ' hui-button-disabled' : '';
+        let buttonCircle = this.props.circle ? ' hui-button-circle' : '';
+        buttonClass += buttonSizeClass + buttonTypeClass + buttonBorderType + buttonDisabled + buttonCircle;
+
         return (
-            <div className="hui-button">
-                按钮
-            </div>
+            <button className={buttonClass}>{this.props.children}</button>
         );
     }
 }
